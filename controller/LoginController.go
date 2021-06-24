e1f9fe3396460bcacd12fbc2a40c6962af525b6b
Noah <noah@zixi.net>
2021-06-25 00:09:39 +0800

    add jwt

---
+++ controller/LoginController.go
@@ -0,0 +1,45 @@
+package controller
+
+import (
+	"gin-skeleton/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+// dto
+type LoginCredentials struct {
+	Email    string `form:"email"`
+	Password string `form:"password"`
+}
+
+//login contorller interface
+type LoginController interface {
+	Login(ctx *gin.Context) string
+}
+
+type loginController struct {
+	loginService service.LoginService
+	jWtService   service.JWTService
+}
+
+func LoginHandler(loginService service.LoginService,
+	jWtService service.JWTService) LoginController {
+	return &loginController{
+		loginService: loginService,
+		jWtService:   jWtService,
+	}
+}
+
+func (controller *loginController) Login(ctx *gin.Context) string {
+	var credential LoginCredentials
+	err := ctx.ShouldBind(&credential)
+	if err != nil {
+		return "no data found"
+	}
+	isUserAuthenticated := controller.loginService.LoginUser(credential.Email, credential.Password)
+	if isUserAuthenticated {
+		return controller.jWtService.GenerateToken(credential.Email, true)
+
+	}
+	return ""
+}
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"gin-skeleton/controller"
+	"gin-skeleton/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello,world")
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	var loginService service.LoginService = service.StaticLoginService()
+	var jwtService service.JWTService = service.JWTAuthService()
+	var loginController controller.LoginController = controller.LoginHandler(loginService, jwtService)
+
+	server := gin.New()
+	server.POST("/login", func(ctx *gin.Context) {
+		token := loginController.Login(ctx)
+		if token != "" {
+			ctx.JSON(http.StatusOK, gin.H{
+				"token": token,
+			})
+		} else {
+			ctx.JSON(http.StatusUnauthorized, nil)
+		}
+	})
+	port := "8080"
+	server.Run(":" + port)
+
 }
